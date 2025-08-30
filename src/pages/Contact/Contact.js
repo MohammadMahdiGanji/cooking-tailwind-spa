7433c9f29cee58515ca042b06d58d40eea9a304c
Mohammad Mahdi Ganji <mohammadmahdiganji1384@gmail.com>
2025-08-30 13:53:07 +0330

    add header section page contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,11 @@
-
+import CreateElement from "../../Component/CreateElement/CreateElement";
+import Header from "../../Component/Header/Header";
+import Append from "../../Component/Append/Append";
 export default function Contact() {
+  const contact = CreateElement({name:"div"})
+  const header = Header()
 
+  Append(contact,header)
 
-  return title;
+  return contact;
 }
