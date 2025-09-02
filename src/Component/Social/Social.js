e53136cdbf8d2457de5be3fac3d0a3ee293ff654
Mohammad Mahdi Ganji <mohammadmahdiganji1384@gmail.com>
2025-09-02 22:37:23 +0330

    social section reform

--- src/Component/Social/Social.js
+++ src/Component/Social/Social.js
@@ -4,52 +4,70 @@ import { dataSocial } from "../../data";
 import Button from "../Button/Button";
 import instagramSrc from "../../assets/icon/instagram.svg";
 import Link from "../LInk/Link";
+import NotFound from "../NotFound/NotFound";
 
 export default function Social() {
-  const social = CreateElement({ name: "section", style:"bg-gradient-to-t from-primary to-white mt-28" });
-  const container = CreateElement({ name: "div", style: "container" });
-  const title = CreateElement({
-    name: "h2",
-    title: "Check out @foodieland on Instagram",
-    style:
-      "text-center font-SemiBold xs:text-[22px] sm:text-[30px] md:text-[35px] lg:text-[48px]",
-  });
-  const caption = CreateElement({
-    name: "p",
-    title:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim ",
-    style:
-      "text-center max-w-[840px] mx-auto text-secondary mt-3 text-xs lg:text-sm xl:text-base",
-  });
-  const wrapper = CreateElement({
-    name: "div",
-    style:
-      `grid gap-y-10 grid-cols-12 place-items-center 
-      gap-5 mt-8 sm:mt-12 md:mt-18 lx:gap-10`,
-  });
-  const btnBox = CreateElement({ name: "div", style: "flex " });
-  const btn = Button({
-    title: "Visit Our Instagram",
-    icon: instagramSrc,
-    style: "mx-auto text-center mt-15 ",
-  });
-  const link = Link({ element: btn, href: 777 });
-
-  Append(social, container);
-  Append(btnBox, link);
-  Append(container, ...[title, caption, wrapper, btnBox]);
-  dataSocial.forEach((social) => {
-    const post = CreateElement({
-      name: "img",
-      src: social,
-      style:
-        "w-full max-w-[400px] col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3",
-      alt: "social",
+  // create variables
+  let social, container, title, caption, wrapper, btnBox, btn, link;
+  try {
+    // create elements
+    social = CreateElement({
+      name: "section",
+      style: "bg-gradient-to-t from-primary to-white mt-28",
     });
 
-    Append(wrapper, post);
+    container = CreateElement({ name: "div", style: "container" });
 
-  });
+    title = CreateElement({
+      name: "h2",
+      title: "Check out @foodieland on Instagram",
+      style:
+        "text-center font-SemiBold xs:text-[22px] sm:text-[30px] md:text-[35px] lg:text-[48px]",
+    });
+
+    caption = CreateElement({
+      name: "p",
+      title:
+        "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim ",
+      style:
+        "text-center max-w-[840px] mx-auto text-secondary mt-3 text-xs lg:text-sm xl:text-base",
+    });
+
+    wrapper = CreateElement({
+      name: "div",
+      style: `grid gap-y-10 grid-cols-12 place-items-center 
+        gap-5 mt-8 sm:mt-12 md:mt-18 lx:gap-10`,
+    });
+
+    btnBox = CreateElement({ name: "div", style: "flex " });
+
+    btn = Button({
+      title: "Visit Our Instagram",
+      icon: instagramSrc,
+      style: "mx-auto text-center mt-15 ",
+    });
+
+    link = Link({ element: btn, href: 777 });
+
+    // append elements
+    Append(social, container);
+    Append(btnBox, link);
+    Append(container, ...[title, caption, wrapper, btnBox]);
+    dataSocial.forEach((social) => {
+      const post = CreateElement({
+        name: "img",
+        src: social,
+        style:
+          "w-full max-w-[400px] col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3",
+        alt: "social",
+      });
+
+      Append(wrapper, post);
+    });
+  } catch (err) {
+    NotFound(`Error Social Section ${err.message}`);
+    throw new Error(`Error Social Section ${err.message}`);
+  }
 
   return social;
 }
