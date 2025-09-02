cc160d7145ad6a0e160804e0bde12f75d14a655d
Mohammad Mahdi Ganji <mohammadmahdiganji1384@gmail.com>
2025-09-02 18:48:15 +0330

    recipe and card section reform

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,6 +2,7 @@ import CreateElement from "../CreateElement/CreateElement";
 import Append from "../Append/Append";
 import timerSrc from "../../assets/icon/Timer.svg";
 import ForkKnifeSrc from "../../assets/icon/ForkKnife.svg";
+import NotFound from "../NotFound/NotFound";
 
 export default function Card({
   img = "",
@@ -9,53 +10,74 @@ export default function Card({
   icon = "",
   color = "",
 } = {}) {
-  const Card = CreateElement({
-    name: "div",
-    style: `p-4 ${color} relative rounded-2xl pb-6 max-w-[400px]`,
-  });
-  const image = CreateElement({
-    name: "img",
-    src: img,
-    style: "w-full rounded-2xl object-cover",
-  });
-  const boxIcon = CreateElement({
-    name: "span",
-    style: `absolute top-8 right-8 bg-white w-12
-    h-12 flex items-center justify-center rounded-full`,
-  });
-  const like = CreateElement({ name: "img", src: icon });
-  const title = CreateElement({
-    name: "h3",
-    title: caption,
-    style: "font-SemiBold mt-5 md:text-[22px]",
-  });
+  // Create Variables
+  let Card,
+    image,
+    boxIcon,
+    like,
+    title,
+    box,
+    munitesBox,
+    munitesIcon,
+    munitesText,
+    healthyBox,
+    healthyIcon,
+    healthyText;
+  try {
+    
+    // Create Elements
+    Card = CreateElement({
+      name: "div",
+      style: `p-4 ${color} relative rounded-2xl pb-6 max-w-[400px]`,
+    });
+    image = CreateElement({
+      name: "img",
+      src: img,
+      style: "w-full rounded-2xl object-cover",
+    });
+    boxIcon = CreateElement({
+      name: "span",
+      style: `absolute top-8 right-8 bg-white w-12
+      h-12 flex items-center justify-center rounded-full`,
+    });
+    like = CreateElement({ name: "img", src: icon });
+    title = CreateElement({
+      name: "h3",
+      title: caption,
+      style: "font-SemiBold mt-5 md:text-[22px]",
+    });
 
-  const box = CreateElement({
-    name: "div",
-    style: "flex itmes-center gap-5 mt-9",
-  });
+    box = CreateElement({
+      name: "div",
+      style: "flex itmes-center gap-5 mt-9",
+    });
 
-  const munitesBox = CreateElement({ name: "div", style: "flex gap-2" });
-  const munitesIcon = CreateElement({ name: "img", src: timerSrc });
-  const munitesText = CreateElement({
-    name: "span",
-    title: "Minutes",
-    style: "text-xs md:text-base font-Medium",
-  });
+    munitesBox = CreateElement({ name: "div", style: "flex gap-2" });
+    munitesIcon = CreateElement({ name: "img", src: timerSrc });
+    munitesText = CreateElement({
+      name: "span",
+      title: "Minutes",
+      style: "text-xs md:text-base font-Medium",
+    });
 
-  const healthyBox = CreateElement({ name: "div", style: "flex gap-2" });
-  const healthyIcon = CreateElement({ name: "img", src: ForkKnifeSrc });
-  const healthyText = CreateElement({
-    name: "span",
-    title: "Healthy",
-    style: "text-xs md:text-base font-Medium",
-  });
+    healthyBox = CreateElement({ name: "div", style: "flex gap-2" });
+    healthyIcon = CreateElement({ name: "img", src: ForkKnifeSrc });
+    healthyText = CreateElement({
+      name: "span",
+      title: "Healthy",
+      style: "text-xs md:text-base font-Medium",
+    });
 
-  Append(Card, ...[image, boxIcon, title, box]);
-  Append(boxIcon, like);
-  Append(box, ...[munitesBox, healthyBox]);
-  Append(munitesBox, ...[munitesIcon, munitesText]);
-  Append(healthyBox, ...[healthyIcon, healthyText]);
+    // add element
+    Append(Card, ...[image, boxIcon, title, box]);
+    Append(boxIcon, like);
+    Append(box, ...[munitesBox, healthyBox]);
+    Append(munitesBox, ...[munitesIcon, munitesText]);
+    Append(healthyBox, ...[healthyIcon, healthyText]);
+  } catch (err) {
+    NotFound(`Error Card Section ${err.message}`);
+    throw new Error(`Error Card Section ${err.message}`);
+  }
 
   return Card;
 }
