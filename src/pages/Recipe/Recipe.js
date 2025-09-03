7ff9b9d3e4a76f1bfb1b860a894951df08496a91
Mohammad Mahdi Ganji <mohammadmahdiganji1384@gmail.com>
2025-09-03 21:24:54 +0330

    add footer section recipes page

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,12 +1,14 @@
 import CreateElement from "../../Component/CreateElement/CreateElement";
 import Header from "../../Component/Header/Header";
 import Append from "../../Component/Append/Append";
+import Footer from "../../Component/Footer/Footer";
 
 export default function Recipe() {
-  const recipe = CreateElement({name:"div"})
-  const header = Header()
+  const recipe = CreateElement({ name: "div" });
+  const header = Header();
+  const footer = Footer();
 
-  Append(recipe, ...[header])
+  Append(recipe, ...[header, footer]);
 
-  return header;
+  return recipe;
 }
