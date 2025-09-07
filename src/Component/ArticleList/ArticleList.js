9a01a2150a72b39578854d042aa3d40bcee08201
Mohammad Mahdi <mohammadmahdiganji1384@gmail.com>
2025-09-07 16:06:02 +0330

    reform aritcles component and articleList component in blog page

--- src/Component/ArticleList/ArticleList.js
+++ src/Component/ArticleList/ArticleList.js
@@ -1,9 +1,9 @@
 import CreateElement from "../CreateElement/CreateElement";
 import Append from "../Append/Append";
-import OtherRecipe from "../OtherRecipe/OtherRecipe";
-
+import NotFound from "../NotFound/NotFound";
 
 export default function ArticleList({ data = "" } = {}) {
+  // create variables
   let articleList,
     article,
     img,
@@ -16,66 +16,71 @@ export default function ArticleList({ data = "" } = {}) {
     userImg,
     userName;
 
-  articleList = CreateElement({
-    name: "div",
-    style: " flex flex-col gap-y-5 items-center xs:gap-y-6",
-  });
-
-  console.log(data);
-
-  data.forEach((item) => {
-    article = CreateElement({
+  try {
+    //  create elelemnts
+    articleList = CreateElement({
       name: "div",
-      style: "max-w-[400px] sm:max-w-full sm:flex sm:gap-5",
+      style: " flex flex-col gap-y-5 items-center xs:gap-y-6",
     });
-    img = CreateElement({
-      name: "img",
-      src: item.blogImg,
-      style: "rounded-xl w-full sm:max-w-[290px] ",
-    });
-    wrapper = CreateElement({ name: "div" });
-    title = CreateElement({
-      name: "h4",
-      title: item.title,
-      style: "font-SemiBold text-xs mt-2 xs:text-base sm:mt-0 md:text-[20px]",
-    });
-    text = CreateElement({
-      name: "p",
-      title: item.text,
-      style: "text-xs text-secondary mt-2 line-clamp-3 sm:text-sm lg:text-base",
-    });
-    box = CreateElement({
-      name: "div",
-      style: `mt-3 flex flex-col gap-2 sm:gap-3 divide-y-1 divide-[rgba(0,0,0,0.1)]
+
+    data.forEach((item) => {
+      article = CreateElement({
+        name: "div",
+        style: "max-w-[400px] sm:max-w-full sm:flex sm:gap-5",
+      });
+      img = CreateElement({
+        name: "img",
+        src: item.blogImg,
+        style: "rounded-xl w-full sm:max-w-[290px] ",
+      });
+      wrapper = CreateElement({ name: "div" });
+      title = CreateElement({
+        name: "h4",
+        title: item.title,
+        style: "font-SemiBold text-xs mt-2 xs:text-base sm:mt-0 md:text-[20px]",
+      });
+      text = CreateElement({
+        name: "p",
+        title: item.text,
+        style:
+          "text-xs text-secondary mt-2 line-clamp-3 sm:text-sm lg:text-base",
+      });
+      box = CreateElement({
+        name: "div",
+        style: `mt-3 flex flex-col gap-2 sm:gap-3 divide-y-1 divide-[rgba(0,0,0,0.1)]
        sm:flex-row sm:divide-x-1 sm:divide-y-0 sm:items-center md:mt-10  xl:mt-14`,
-    });
-    userBox = CreateElement({
-      name: "div",
-      style: "flex gap-1 items-center pb-2 sm:pb-0 pr-3 sm:gap-3",
-    });
-    userImg = CreateElement({
-      name: "img",
-      src: item.userIcon,
-      style: "w-6 h-6 sm:w-10 sm:h-10",
-    });
-    userName = CreateElement({
-      name: "span",
-      title: item.name,
-      style: "text-xs font-SemiBold sm:text-sm",
-    });
-    date = CreateElement({
-      name: "span",
-      title: item.date,
-      style: "text-xs text-secondary sm:text-sm",
-    });
+      });
+      userBox = CreateElement({
+        name: "div",
+        style: "flex gap-1 items-center pb-2 sm:pb-0 pr-3 sm:gap-3",
+      });
+      userImg = CreateElement({
+        name: "img",
+        src: item.userIcon,
+        style: "w-6 h-6 sm:w-10 sm:h-10",
+      });
+      userName = CreateElement({
+        name: "span",
+        title: item.name,
+        style: "text-xs font-SemiBold sm:text-sm",
+      });
+      date = CreateElement({
+        name: "span",
+        title: item.date,
+        style: "text-xs text-secondary sm:text-sm",
+      });
 
-
-    Append(articleList, article);
-    Append(article, ...[img, wrapper]);
-    Append(wrapper, ...[title, text, box]);
-    Append(box, ...[userBox, date]);
-    Append(userBox, ...[userImg, userName]);
-  });
+      // add elements
+      Append(articleList, article);
+      Append(article, ...[img, wrapper]);
+      Append(wrapper, ...[title, text, box]);
+      Append(box, ...[userBox, date]);
+      Append(userBox, ...[userImg, userName]);
+    });
+  } catch (err) {
+    NotFound(`Error Article List Component ${err.message}`);
+    throw new Error(`Error Article List Component ${err.message}`);
+  }
 
   return articleList;
 }
--- src/Component/Articles/Articles.js
+++ src/Component/Articles/Articles.js
@@ -5,8 +5,10 @@ import ArticleList from "../ArticleList/ArticleList";
 import OtherRecipe from "../OtherRecipe/OtherRecipe";
 import { dataOtherRecipe } from "../../data";
 import foodSrc from "../../assets/images/food.png";
+import NotFound from "../NotFound/NotFound";
 
 export default function Articles() {
+  // create variables
   let articles,
     container,
     wrapper,
@@ -16,31 +18,38 @@ export default function Articles() {
     otherRecipe,
     imgFood;
 
-  articles = CreateElement({ name: "section", style: "mt-20" });
-  container = CreateElement({ name: "div", style: "container" });
-  wrapper = CreateElement({ name: "div", style: "grid grid-cols-12 gap-5" });
-  article = CreateElement({
-    name: "article",
-    style: "col-span-12 lg:col-span-8",
-  });
-  recipe = CreateElement({ name: "div", style: "col-span-12 lg:col-span-4" });
-  articleList = ArticleList({ data: dataArticle });
-  otherRecipe = OtherRecipe({
-    title: "Tasty Recipes",
-    data: dataOtherRecipe,
-    style: `sm:items-start sm:max-w-[500px] lg:max-w-[300px] sm:mt-8 lg:mt-0`,
-  });
-  imgFood = CreateElement({
-    name: "img",
-    src: foodSrc,
-    style: "max-w-[350px] lg:max-w-[300px] block mx-auto  mt-10 lg:mt-16",
-  });
+  try {
+    // create elements
+    articles = CreateElement({ name: "section", style: "mt-20" });
+    container = CreateElement({ name: "div", style: "container" });
+    wrapper = CreateElement({ name: "div", style: "grid grid-cols-12 gap-5" });
+    article = CreateElement({
+      name: "article",
+      style: "col-span-12 lg:col-span-8",
+    });
+    recipe = CreateElement({ name: "div", style: "col-span-12 lg:col-span-4" });
+    articleList = ArticleList({ data: dataArticle });
+    otherRecipe = OtherRecipe({
+      title: "Tasty Recipes",
+      data: dataOtherRecipe,
+      style: `sm:items-start sm:max-w-[500px] lg:max-w-[300px] sm:mt-8 lg:mt-0`,
+    });
+    imgFood = CreateElement({
+      name: "img",
+      src: foodSrc,
+      style: "max-w-[350px] lg:max-w-[300px] block mx-auto  mt-10 lg:mt-16",
+    });
 
-  Append(articles, container);
-  Append(container, wrapper);
-  Append(wrapper, ...[article, recipe]);
-  Append(article, articleList);
-  Append(recipe, ...[otherRecipe, imgFood]);
+    // add Elements
+    Append(articles, container);
+    Append(container, wrapper);
+    Append(wrapper, ...[article, recipe]);
+    Append(article, articleList);
+    Append(recipe, ...[otherRecipe, imgFood]);
+  } catch (err) {
+    NotFound(`Error Article section to blog page ${err.message}`);
+    throw new Error(`Error Article section to blog page ${err.message}`);
+  }
 
   return articles;
 }
