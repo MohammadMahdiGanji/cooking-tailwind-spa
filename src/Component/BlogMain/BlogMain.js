43b175e3274caf6b52cd2400fa9315349287a877
Mohammad Mahdi <mohammadmahdiganji1384@gmail.com>
2025-09-07 16:09:38 +0330

    add inbox section to blog page

--- src/Component/BlogMain/BlogMain.js
+++ src/Component/BlogMain/BlogMain.js
@@ -1,14 +1,16 @@
 import CreateElement from "../CreateElement/CreateElement";
 import Append from "../Append/Append";
 import Articles from "../Articles/Articles";
+import Inbox from "../Inbox/Inbox";
 
 export default function BlogMain() {
-  let blogMain, articles;
+  let blogMain, articles,inbox;
 
   blogMain = CreateElement({ name: "main" });
   articles = Articles();
+  inbox = Inbox()
 
-  Append(blogMain, ...[articles]);
+  Append(blogMain, ...[articles, inbox]);
 
   return blogMain;
 }
