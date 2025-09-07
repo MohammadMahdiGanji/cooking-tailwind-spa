9d79f2ca8c581b4f7df783009acc0b24c2871a76
Mohammad Mahdi <mohammadmahdiganji1384@gmail.com>
2025-09-07 12:38:44 +0330

    add footer section to blog page

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,12 +1,14 @@
 import CreateElement from "../../Component/CreateElement/CreateElement";
 import Append from "../../Component/Append/Append";
 import Header from "../../Component/Header/Header"
+import Footer from "../../Component/Footer/Footer";
 
 export default function Blog() {
+
   const blog = CreateElement({name:'div'})
-
   const header = Header()
+  const footer = Footer()
 
-  Append(blog,...[header])
+  Append(blog,...[header, footer])
   return blog;
 }
