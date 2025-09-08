df85f2b1ce251e983ba49cfa4c73c3c09102a957
Mohammad Mahdi <mohammadmahdiganji1384@gmail.com>
2025-09-08 12:51:35 +0330

    add inbox section to content page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,14 +3,17 @@ import Header from "../../Component/Header/Header";
 import Append from "../../Component/Append/Append";
 import Footer from "../../Component/Footer/Footer";
 import ContactContent from "../../Component/ContactContent/ContactContent";
+import Inbox from "../../Component/Inbox/Inbox";
+
 
 export default function Contact() {
   const contact = CreateElement({ name: "div" });
   const header = Header();
   const contactContent = ContactContent();
+  const inbox = Inbox();
   const footer = Footer();
-
-  Append(contact, ...[header, contactContent, footer]);
+  
+  Append(contact, ...[header, contactContent, inbox, footer]);
 
   return contact;
 }
