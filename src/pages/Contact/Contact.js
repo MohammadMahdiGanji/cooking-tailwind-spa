5208044c2156aed3c52d52289b669b60558ea7db
Mohammad Mahdi <mohammadmahdiganji1384@gmail.com>
2025-09-08 10:52:54 +0330

    add footer section to contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,14 @@
 import CreateElement from "../../Component/CreateElement/CreateElement";
 import Header from "../../Component/Header/Header";
 import Append from "../../Component/Append/Append";
+import Footer from "../../Component/Footer/Footer";
+
 export default function Contact() {
   const contact = CreateElement({ name: "div" });
   const header = Header();
+  const footer = Footer();
 
-  Append(contact, ...[header]);
+  Append(contact, ...[header, footer]);
 
   return contact;
 }
